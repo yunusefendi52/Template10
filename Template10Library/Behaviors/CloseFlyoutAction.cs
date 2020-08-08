95af1622bca7770609eb8a98cbffde09b670b4ee
Yunus <yunus.efendin97@outlook.com>
2020-08-08 11:30:53 +0700

    Add initial uno support

---
+++ Template10Library/Behaviors/CloseFlyoutAction.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace Template10.Behaviors
+{
+    public partial class CloseFlyoutAction : DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            var parent = TargetObject ?? sender as DependencyObject;
+            while (parent != null)
+            {
+                if (parent is FlyoutPresenter)
+                {
+                    throw new NotImplementedException("IsOpen not implemented");
+                }
+                else
+                {
+                    parent = VisualTreeHelper.GetParent(parent);
+                }
+            }
+            return null;
+        }
+
+        public Control TargetObject
+        {
+            get { return (Control)GetValue(TargetObjectProperty); }
+            set { SetValue(TargetObjectProperty, value); }
+        }
+        public static readonly DependencyProperty TargetObjectProperty =
+            DependencyProperty.Register(nameof(TargetObject), typeof(Control), typeof(CloseFlyoutAction), new PropertyMetadata(null));
+    }
+}
---
+++ Template10Library/Behaviors/TextBoxEnterKeyBehavior.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Template10.Behaviors
+{
+    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-XamlBehaviors
+    [ContentProperty(Name = nameof(Actions))]
+    [TypeConstraint(typeof(TextBox))]
+    [Obsolete("Use KeyBehavior instead.")]
+    public partial class TextBoxEnterKeyBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+            (AssociatedObject as TextBox).KeyUp += AssociatedTextBox_KeyUp;
+        }
+
+        public void Detach()
+        {
+            (AssociatedObject as TextBox).KeyUp -= AssociatedTextBox_KeyUp;
+        }
+
+        private void AssociatedTextBox_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Interaction.ExecuteActions(AssociatedObject, Actions, null);
+                e.Handled = true;
+            }
+        }
+
+        public ActionCollection Actions
+        {
+            get
+            {
+
+#if NETFX_CORE
+                var actions = (ActionCollection)base.GetValue(ActionsProperty);
+#else
+                var actions = (ActionCollection)GetValue(ActionsProperty);
+#endif
+                if (actions == null)
+                {
+                    SetValue(ActionsProperty, actions = new ActionCollection());
+                }
+                return actions;
+            }
+        }
+        public static readonly DependencyProperty ActionsProperty =
+            DependencyProperty.Register(nameof(Actions), typeof(ActionCollection),
+                typeof(TextBoxEnterKeyBehavior), new PropertyMetadata(null));
+    }
+}
